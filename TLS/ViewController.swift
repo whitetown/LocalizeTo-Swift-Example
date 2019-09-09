0810b5f25905d63b1f603d0c21bbf957fc5bafcc
Sergey Chehuta <whitetownmail@gmail.com>
2019-09-09 14:30:15 +0200

    Initial Commit

---
+++ TLS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TLS
+//
+//  Created by Sergey Chehuta on 09/09/2019.
+//  Copyright © 2019 WhiteTown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
